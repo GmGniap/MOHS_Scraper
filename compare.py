e21cfc113735a6382d86253d289a2b6df128da2e
gmgniap <thetpaimyo103@gmail.com>
2021-01-19 01:30:34 +0630

    prepare for pcode merge

--- compare.py
+++ compare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 import datetime
 
+
 engine = create_engine('mysql+pymysql://root:paing@localhost:3306/mohs')
 ts_read = pd.read_sql('SELECT * FROM township',con=engine)
 #print(ts_read.head())
@@ -20,10 +21,10 @@ ts_read = pd.read_sql('SELECT * FROM township',con=engine)
 #ts_read['sr'] = ts_read['sr'].str.replace(r'Naypyitaw\*', 'NPT')
 
 ts_read = ts_read.replace({'sr': r'\*'}, {'sr': ''}, regex=True)
-ts_read.to_csv(r'.\township.csv')
+#ts_read.to_csv(r'.\township.csv')
 #print(ts_read.columns)
 #sample_ts = ts_read.iloc[:50, 3:5]
-sample_ts = ts_read[:50]
+
 #print(pd.unique(ts_read['sr']))
 #print(len(pd.unique(ts_read['sr'])))
 
@@ -38,8 +39,8 @@ group_test = ts_read.groupby('trouble_day').size()
 print(group_test)
 ''' 
 ## To filter date , used datetime.date funtion 
-sep_date = ts_read[ts_read['upload_date'] == datetime.date(2021,1,16)]
-
+sep_date = ts_read[ts_read['upload_date'] == datetime.date(2021,1,18)]
+sample_ts = sep_date[['townships','sr']].copy()
 ## Take datframe Yangon Region only & Date-Jan16 
 #ygn = ts_read[(ts_read['sr']=='Yangon Region') & (ts_read['upload_date'] == datetime.date(2021,1,16))]
 #print(ygn)
@@ -48,5 +49,32 @@ sep_date = ts_read[ts_read['upload_date'] == datetime.date(2021,1,16)]
 
 
 jan_16 = sep_date.groupby('sr')['cumulative_no']
-print(jan_16.sum())
+#print(jan_16.sum())
 #print(g.transform(lambda x: x.rank(ascending=False)))
+
+## Read Pcode excel file 
+#xls = pd.ExcelFile("pcode_9_2.xlsx")
+#df1 = pd.read_excel(xls, '03_Township', engine='openpyxl')
+cols_to_keep = ['Township_Name_Eng','Tsp_Pcode','SR_Pcode','SR_Name_Eng','Township_Name_MMR']
+df1 = pd.read_excel('./pcode_9_2.xlsx', sheet_name='03_Township', engine='openpyxl')
+df1 = df1[cols_to_keep]
+#print(df1.head())
+#print(df1.columns)
+
+## take only first value of the words - example: Botahtaung Township >> Botahtaung , YGN Region >> YGN 
+remove_pat = re.compile(r'Township')
+sr_pat = re.compile(r'Region|State')
+#sample_ts['First_ts'] = sample_ts['townships'].str.rsplit(' ', expand=False)
+sample_ts['First_ts'] = sample_ts.loc[:,'townships'].str.replace(remove_pat, '').str.strip()
+sample_ts['First_sr'] = sample_ts.loc[:,'sr'].str.replace(sr_pat, '').str.strip()
+#sample_ts[:,'First_sr'].str.strip()
+#print(sample_ts.shape)
+
+## Remove row that contain unnecessary values 
+sample_ts = sample_ts[~sample_ts.First_sr.str.contains("New Case")]
+#print(sample_ts.shape)
+sample_ts['First_sr'].replace({
+    ""
+})
+print(pd.unique(sample_ts['First_sr']))
+print(pd.unique(df1['SR_Name_Eng']))
